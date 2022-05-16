7ef7c3460745298998a33b44ea0638b894df171a
Tomasz Ciechanowski <ciechantako@gmail.com>
2022-05-16 23:22:36 +0200

    add file explorer

--- Assets/Scripts/FileExplorer.cs
+++ Assets/Scripts/FileExplorer.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class FileExplorer : MonoBehaviour
 {
     private string path;
-    private void Start()
+    public void OnButtonClick()
     {
-        path = DataConverter.Instance.Dir.ToString(); 
+        path = DataConverter.Instance.Dir.ToString();
+        EditorUtility.OpenFilePanel("Image Explorer", path, "png");
     }
 }
