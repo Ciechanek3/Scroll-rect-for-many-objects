60554e855ce0cd22a6b459a43252aeaa5220f678
Tomasz Ciechanowski <ciechantako@gmail.com>
2022-05-16 19:06:36 +0200

    speed up sprite creation

--- Assets/Scripts/DataConverter.cs
+++ Assets/Scripts/DataConverter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class DataConverter : MonoBehaviour
@@ -11,7 +12,6 @@ public class DataConverter : MonoBehaviour
     public static DataConverter Instance;
 
     public event Action OnRefreshButtonPressed;
-
     public DateTime startUpDateTime;
     private FileInfo[] info;
     private DirectoryInfo dir;
@@ -42,16 +42,16 @@ public class DataConverter : MonoBehaviour
     }
     public ImageData GetSpecificElement(int index)
     {
-        Texture2D texture = new Texture2D(1, 1, TextureFormat.Alpha8, false);
-        byte[] bytes = File.ReadAllBytes(dir.ToString() + info[index].Name);
+        Texture2D texture = new Texture2D(1, 1);
+        byte[] bytes = File.ReadAllBytes(info[index].FullName);
         var name = info[index].Name.Substring(0, info[index].Name.IndexOf("."));
-        texture.LoadImage(bytes); // Resources.Load for sprite removes lags in app but it doesn't work on build
-        var sprite = Sprite.Create(texture, new Rect(new Vector2(0, 0), new Vector2(texture.width, texture.height)), new Vector2(0, 0));
+        texture.LoadImage(bytes);
+        Sprite sprite = Sprite.Create(texture, new Rect(new Vector2(0, 0), new Vector2(texture.width, texture.height)), new Vector2(0, 0), 1, 0, SpriteMeshType.FullRect);
         return new ImageData(sprite, name, info[index].CreationTime);
     }
     public void OnButtonPressed()
     {
-        startUpDateTime = DateTime.Now; // i changed it because i think its cleaner when time since created is shown for same second instead of seconds of delay because of enabling objects
+        startUpDateTime = DateTime.Now;
         CreateDataList();
         OnRefreshButtonPressed?.Invoke();
     }
